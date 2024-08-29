b69f4c377c99b578dc621e1eb0fbc8b9c3e3f293
Rajat Rohilla <143750891+Rajat-Rohilla@users.noreply.github.com>
2024-08-29 15:58:22 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("📄 Document question answering")
+st.title("📄 My Document question answering")
 st.write(
     "Upload a document below and ask a question about it – GPT will answer! "
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
