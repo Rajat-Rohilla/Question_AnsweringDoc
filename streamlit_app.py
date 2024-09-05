f4b9a11c39fa83bc2aba46e979d6ea64feeac987
Rajat Rohilla <143750891+Rajat-Rohilla@users.noreply.github.com>
2024-09-05 14:43:40 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import openai
 from openai import OpenAI
 import streamlit as st
 
-lab1 = st.Page("LAB1", title="First lab")
-lab2 = st.Page("LAB2", title="Second lab",default=True)
+LAB1 = st.Page("LAB1", title="First lab")
+LAB2 = st.Page("LAB2", title="Second lab",default=True)
 
-pg= st.navigation([lab1,lab2])
-pg.run
+pg = st.navigation([LAB1,LAB2])
+pg.run()
