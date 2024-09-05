580dd1fb1b4e2d6868fb5b6e2ad23fdbf115c6de
Rajat Rohilla <143750891+Rajat-Rohilla@users.noreply.github.com>
2024-09-05 14:48:48 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,7 @@
-# Chatgpt 4.0 mini powered document Answesering 
-import openai
-from openai import OpenAI
 import streamlit as st
 
-LAB1 = st.Page("LAB1", title="First lab")
-LAB2 = st.Page("LAB2", title="Second lab",default=True)
+Lab1_page = st.Page("LAB1.py", title="First lab")
+Lab2_page = st.Page("LAB2.py", title="Second lab",default=True)
 
-pg = st.navigation([LAB1,LAB2])
+pg = st.navigation([Lab1_page,Lab2_page])
 pg.run()
