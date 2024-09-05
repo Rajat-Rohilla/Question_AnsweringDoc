aab0345c1de40e373e41c2ac8c6740af62a62457
Rajat Rohilla <143750891+Rajat-Rohilla@users.noreply.github.com>
2024-09-05 14:58:38 +0000

    COMMIT

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 # Sidebar for navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Lab1", "Lab2"], index=1)  # Setting 'Lab2' as default with index=1
+page = st.sidebar.radio("Go to", ["Lab1", "Lab2"], index=1)  # Set Lab2 as the default page
 
-# Logic to display the appropriate page
+# Toggle between different .py files
 if page == "Lab1":
-    st.title("Welcome to Lab 1")
-    st.write("This is the content for Lab 1.")
+    st.title("Lab 1")
+    exec(open("LAB1.py").read())  # Executes code from lab1.py
 elif page == "Lab2":
-    st.title("Welcome to Lab 2")
-    st.write("This is the default page (Lab 2).")
+    st.title("Lab 2")
+    exec(open("LAB2.py").read()) 
