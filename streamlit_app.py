b863718c3722d9e0dd1f0456019b75199ecd59e8
Rajat Rohilla <143750891+Rajat-Rohilla@users.noreply.github.com>
2024-09-06 02:54:48 +0000

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,6 @@ import streamlit as st
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Go to", ["Lab1", "Lab2"], index=1)  # Set Lab2 as the default page
 
-# Sidebar option for document summary generation
-st.sidebar.title("Document Summary Options")
-summary_option = st.sidebar.radio(
-    "Choose a summary type:",
-    ["Summarize in 100 words", "Summarize in 2 paragraphs", "Summarize in 5 bullet points"]
-)
-
 # Toggle between different .py files
 if page == "Lab1":
     st.title("Lab 1")
